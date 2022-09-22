0daaa566eb486e9728b948094d91b1c233028430
Chrisi2209 <christopher.rodax@gmail.com>
2022-09-22 10:27:48 +0200

    Initial commit

---
+++ ConsoleAdventureGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAdventureGame
+{
+    class Program
+    {
+        /* Stilvorschriften
+         * 
+         * Variablen: klein anfangen, weitere Wörter aneinander mit Großbuchstaben (camelcase) bsp: ichBinCool
+         * Methoden, Klassen: Groß anfangen, weitere Wörter aneinander mit Großbuchstaben (pascalcase) bsp: IchBinCool
+         * eigene Zeile für geschwungene Klammern 
+         * 
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
